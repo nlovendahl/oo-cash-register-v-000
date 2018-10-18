5e13d245d61068352db98ac834390f8cbd9cd135
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-18 17:45:04 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,6 +16,7 @@ class CashRegister
     if discount != 0
     self.total = (@total - (@total * (@discount * 0.01))).to_i
     puts "After the discount, the total comes to $#{self.total}."
+  else
   end
 
 end
